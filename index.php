7b552efba893cf34c9ef3b73c49f296efd9b2e72
Admin <Admin@zvezdi>
2017-05-25 14:37:35 +0300

    Merge origin/master
    Conflicts:
    	index.php

--- index.php
+++ index.php
@@ -11,9 +11,20 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-        echo "Helo word";
-        echo "Hi";
-        echo "By";
+        $probna = isset($_POST['promenliva']) ? $_POST['promenliva'] : "niama promenliva v post";
+        echo $probna. '<br/>';
+        
+        
+        $year = 2011;
+        
+        switch($year){
+            case 2000: echo "godina 2000"; break;
+            case 2001: echo "godina 2001"; break;
+            case 2010: echo "godina 2010"; break;
+            case 2011: echo "godina 2011"; break;
+            case 2015: echo "godina 2015"; break;
+        }
+          
         ?>
     </body>
 </html>
